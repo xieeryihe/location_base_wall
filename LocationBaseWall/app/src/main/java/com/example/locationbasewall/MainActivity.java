8cf1cb8ad3ca820e3e7edc9d1c8e654881c38943
xieeryihe <2640182777@qq.com>
2023-05-29 23:37:21 +0800

    home push test

--- LocationBaseWall/app/src/main/java/com/example/locationbasewall/MainActivity.java
+++ LocationBaseWall/app/src/main/java/com/example/locationbasewall/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
+
         });
     }
 }
